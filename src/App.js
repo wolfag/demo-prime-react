86373643b0db18358ae416c92cc37b7cef86e26d
TaiNguyen <tai@useget.com>
2020-06-08 20:00:17 +0700

    config prime

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { Button } from 'primereact/button';
 import logo from './logo.svg';
 import './App.css';
 
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 function App() {
+  const [count, setCount] = useState(0);
+  const increment = useCallback(() => {
+    setCount(count + 1);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Button label='Click me' icon='pi pi-check' onClick={increment} />
+      <p>Number pf click: {count}</p>
     </div>
   );
 }
